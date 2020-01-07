4381edba4beefd3f403e1410ec3f80889563b8b4
Carly Yuriko <cyuriko@oa-k2.21-school.ru>
2020-01-07 19:00:10 +0300

    Some fix

--- sources/main.c
+++ sources/main.c
@@ -6,7 +6,7 @@
 /*   By: sleonia <sleonia@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/25 17:34:49 by cyuriko           #+#    #+#             */
-/*   Updated: 2020/01/07 18:22:40 by cyuriko          ###   ########.fr       */
+/*   Updated: 2020/01/07 18:34:17 by cyuriko          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,13 +26,15 @@ int 	main()
 {
 	t_env	*env;
 	
-	remove("sleonia.txt");
-	fd = open("sleonia.txt", O_CREAT);
-	system("chmod 777 sleonia.txt");
-	fd = open("sleonia.txt", O_WRONLY);
+//	remove("sleonia.txt");
+//	fd = open("sleonia.txt", O_CREAT);
+//	system("chmod 777 sleonia.txt");
+//	fd = open("sleonia.txt", O_WRONLY);
 
 	if (!(env = init_env()))
 		return (int_error(ERROR_MALLOC));
+	while (1) {
+
 	if (!(parse_player(env)))
 		return (int_error(ERROR_INPUT));
 	if (!(parse_plateau(env)))
@@ -41,29 +43,30 @@ int 	main()
 	init_heatmap(env);
 	parse_piece(env->piece);
 //	cropped_map(env->piece);
-	for (size_t k = 0; k < env->piece->height; k++)
-	{
-		for (size_t i = 0; i < env->piece->width; i++)
-		{
-			ft_putnbr_fd(env->piece->piece_map[k][i], fd);
-			ft_putchar_fd(' ', fd);
-		}
-		ft_putchar_fd('\n', fd);
-	}
-	ft_putstr_fd("heatmap\n", fd);
-	 for (size_t k = 0; k < env->map->height; k++)
-	 {
-	 	for (size_t i = 0; i < env->map->width; i++)
-	 	{
-	 		ft_putnbr_fd(env->map->heatmap[k][i], fd);
-	 		 ft_putchar_fd(' ', fd);
-	 	}
-	 	ft_putchar_fd('\n', fd);
-	 }
+//	for (size_t k = 0; k < env->piece->height; k++)
+//	{
+//		for (size_t i = 0; i < env->piece->width; i++)
+//		{
+//			ft_putnbr_fd(env->piece->piece_map[k][i], fd);
+//			ft_putchar_fd(' ', fd);
+//		}
+//		ft_putchar_fd('\n', fd);
+//	}
+//	ft_putstr_fd("heatmap\n", fd);
+//	 for (size_t k = 0; k < env->map->height; k++)
+//	 {
+//	 	for (size_t i = 0; i < env->map->width; i++)
+//	 	{
+//	 		ft_putnbr_fd(env->map->heatmap[k][i], fd);
+//	 		 ft_putchar_fd(' ', fd);
+//	 	}
+//	 	ft_putchar_fd('\n', fd);
+//	 }
 	env->best_x = -1;
 	env->best_y = -1;
 	if (try_it_out(env))
 		print_coords(env->best_x, env->best_y);
+	}
 
 // void	print_coords(t_filler *filler)
 // {
@@ -73,25 +76,25 @@ int 	main()
 // 	ft_putchar('\n');
 // }
 // 	}
-	ft_putstr_fd("env->best_x ", fd);
-	ft_putnbr_fd(env->best_x, fd);
-	ft_putchar_fd('\n', fd);
-	ft_putstr_fd("env->best_y ", fd);
-	ft_putnbr_fd(env->best_y, fd);
-	ft_putchar_fd('\n', fd);
- 	ft_putchar_fd('\n', fd);
-	ft_putstr_fd("env->map->height ", fd);
-	ft_putnbr_fd(env->map->height, fd);
-	ft_putchar_fd('\n', fd);
-	ft_putstr_fd("env->map->width ", fd);
-	ft_putnbr_fd(env->map->width, fd);
-ft_putchar_fd('\n', fd);
-	ft_putstr_fd("env->piece->height ", fd);
-	ft_putnbr_fd(env->piece->height, fd);
-	ft_putchar_fd('\n', fd);
-	ft_putstr_fd("env->piece->width ", fd);
-	ft_putnbr_fd(env->piece->width, fd);
-	ft_putchar_fd('\n', fd);
+//	ft_putstr_fd("env->best_x ", fd);
+//	ft_putnbr_fd(env->best_x, fd);
+//	ft_putchar_fd('\n', fd);
+//	ft_putstr_fd("env->best_y ", fd);
+//	ft_putnbr_fd(env->best_y, fd);
+//	ft_putchar_fd('\n', fd);
+// 	ft_putchar_fd('\n', fd);
+//	ft_putstr_fd("env->map->height ", fd);
+//	ft_putnbr_fd(env->map->height, fd);
+//	ft_putchar_fd('\n', fd);
+//	ft_putstr_fd("env->map->width ", fd);
+//	ft_putnbr_fd(env->map->width, fd);
+//ft_putchar_fd('\n', fd);
+//	ft_putstr_fd("env->piece->height ", fd);
+//	ft_putnbr_fd(env->piece->height, fd);
+//	ft_putchar_fd('\n', fd);
+//	ft_putstr_fd("env->piece->width ", fd);
+//	ft_putnbr_fd(env->piece->width, fd);
+//	ft_putchar_fd('\n', fd);
 //	ft_putnbr_fd(env->best_x, fd);
 //	ft_putchar_fd(' ', fd);
 //	ft_putnbr_fd(env->best_y, fd);
--- sources/put_piece.c
+++ sources/put_piece.c
@@ -6,7 +6,7 @@
 /*   By: sleonia <sleonia@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/28 05:30:04 by sleonia           #+#    #+#             */
-/*   Updated: 2020/01/07 18:25:56 by cyuriko          ###   ########.fr       */
+/*   Updated: 2020/01/07 18:54:01 by cyuriko          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,35 @@
 
 extern int fd;
 
-static bool		put_piece(t_env *env, int h, int w)
+static	int 	count_me_shit(t_piece *piece)
+{
+	int shit;
+	int h;
+	int w;
+
+	shit = 0;
+	h = -1;
+	while (++h < piece->height)
+	{
+		w = -1;
+		while (++w < piece->width)
+		{
+			if (piece->piece_map[h][w] == 1)
+				shit++;
+		}
+	}
+	return (shit);
+}
+
+static bool		put_piece(t_env *env, int h, int w, int shit)
 {
 	int	h_counter;
 	int w_counter;
 	int	crosscounter;
 	int max_heat;
+	int cum;
 
+	cum = 0;
 	crosscounter = 0;///////////счетчик пересечениц
 	h_counter = -1;
 	max_heat = 0;
@@ -29,31 +51,36 @@ static bool		put_piece(t_env *env, int h, int w)
 		w_counter = -1;
 		while (++w_counter < env->piece->width)
 		{
-			if (env->map->heatmap[h + h_counter][w + w_counter] == OPPONENT_HEAT && env->piece->piece_map[h_counter][w_counter] == 1)
+			if (w + w_counter < env->map->width && h + h_counter < env->map->height)
 			{
-				ft_putstr_fd("SASI HUI", fd);
-				return (false);
+				if (env->map->heatmap[h + h_counter][w + w_counter] == OPPONENT_HEAT && env->piece->piece_map[h_counter][w_counter] == 1)
+				{
+					ft_putstr_fd("SASI HUI", fd);
+					return (false);
+				}
+
+				if (env->map->heatmap[h + h_counter][w + w_counter] == MY_HEAT && env->piece->piece_map[h_counter][w_counter] == 1)//
+				{
+					++crosscounter;
+					cum++;
+				}
+				if (env->piece->piece_map[h_counter][w_counter] == 1 && env->map->heatmap[h + h_counter][w + w_counter] > max_heat)
+				{
+					max_heat = env->map->heatmap[h + h_counter][w + w_counter];
+					cum++;
+					ft_putstr_fd("max_heat ", fd);
+					ft_putnbr_fd(max_heat, fd);
+					ft_putchar_fd('\n', fd);
+				}
 			}
 
-			if (env->map->heatmap[h + h_counter][w + w_counter] == MY_HEAT && env->piece->piece_map[h_counter][w_counter] == 1)//
-			{
-				++crosscounter;
-
-			}
-			if (env->piece->piece_map[h_counter][w_counter] == 1 && env->map->heatmap[h + h_counter][w + w_counter] > max_heat)
-			{
-				max_heat = env->map->heatmap[h + h_counter][w + w_counter];
-				ft_putstr_fd("max_heat ", fd);
-				ft_putnbr_fd(max_heat, fd);
-				ft_putchar_fd('\n', fd);
-			}
 
 		}
 	}
 	ft_putstr_fd("crosscounter ", fd);
 	ft_putnbr_fd(crosscounter, fd);
 	ft_putchar_fd('\n', fd);
-	if (max_heat > env->max_heat && crosscounter == 1) //тут что-то менял
+	if (max_heat > env->max_heat && crosscounter == 1 && shit == cum) //тут что-то менял
 	{
 		env->max_heat = max_heat;
 		env->best_y = h;
@@ -74,15 +101,17 @@ bool		try_it_out(t_env *env) //хуево работает
 {
 	int	h;
 	int	w;
+	int shit;
 
 	h = -1;
+	shit = count_me_shit(env->piece);
 	env->max_heat = 0;
-	while (++h < env->map->height - env->piece->height + 1)
+	while (++h < env->map->height)
 	{
 		w = -1;
-		while (++w < env->map->width - env->piece->width + 1)
+		while (++w < env->map->width)
 		{
-			put_piece(env, h, w);
+			put_piece(env, h, w, shit);
 	//		if (!(p))
 	//			return (false);
 		}
